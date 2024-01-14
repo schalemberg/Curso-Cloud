0c5d1375b1db2ce0d2c63de13e82a89a62817034
Leandro Schalemberg <106849844+schalemberg@users.noreply.github.com>
2024-01-14 20:16:59 -0300

    Add files via upload

---
+++ TemplateStrings/script.js
@@ -0,0 +1,12 @@
+const num1 = 4;
+const num2 = 5;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let resultado = `A soma de ${num1} + ${num2} é ${soma}
+e a subtração é ${subtracao}
+e a multiplicação é ${multiplicacao}
+e a divisão é ${divisao}`;
+console.log(resultado);
