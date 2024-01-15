6dd8e7916745dfcdfd7d2f41139aed49150da3e3
Leandro Schalemberg <106849844+schalemberg@users.noreply.github.com>
2024-01-14 21:48:36 -0300

    Add files via upload

--- CreateElement/script.js
+++ CreateElement/script.js
@@ -2,23 +2,13 @@ let adicionaElemento = document.createElement("h1");
 adicionaElemento.innerText = "Exercício - JavaScript DOM";
 adicionaElemento.id = "titulo";
 
+//método complexo
 let produto = document.createElement("div");
-produto.id = "produto";
+produto.innerHTML = `<h2>Notebook Dell</h2>
+<img src="NotebookDell.jpg" id="imagem" />
+<p>Exelente notebook para trabalho e estudos, com processador i7 e 16GB de RAM</p>`;
 
-let nomeProduto = document.createElement("h2");
-nomeProduto.textContent = "Notebook Dell";
-produto.appendChild(nomeProduto);
-
-let imagemProduto = document.createElement("img");
-imagemProduto.src = "NotebookDell.jpg";
-imagemProduto.id = "imagem";
-produto.appendChild(imagemProduto);
-
-let descricaoProduto = document.createElement("p");
-descricaoProduto.textContent =
-  "Exelente notebook para trabalho e estudos, com processador i7 e 16GB de RAM";
-produto.appendChild(descricaoProduto);
-
+//método simples
 let precoProduto = document.createElement("p");
 precoProduto.textContent = "Preço: R$ 4.500,00";
 produto.appendChild(precoProduto);
